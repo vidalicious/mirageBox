6d11a415b445589a7891cc9e5c7ce189e3a2bead
wdgu <wdgu@Ctrip.com>
2015-12-07 14:57:28 +0800

    create world

---
+++ mirageBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mirageBox
+//
+//  Created by vidalicious on 12/7/15.
+//  Copyright © 2015 vidalicious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mirageBox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mirageBox
+//
+//  Created by vidalicious on 12/7/15.
+//  Copyright © 2015 vidalicious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
